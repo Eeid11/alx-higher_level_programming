0f09f29049a83f07831d02ea8bff750a1140d4d5
Eeid11 <eeid02388@gmail.com>
2024-04-10 15:31:00 -0700

    es

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// scribit to print loop
+
+let i;
+const string = ['c is fun', 'python is cool', 'javascript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(string[i]);
+}
