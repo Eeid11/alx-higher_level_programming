fede40eff6f148bafe09ab56cfff327fae820498
Eeid11 <eeid02388@gmail.com>
2024-04-10 15:34:01 -0700

    esla

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
 // scribit to print loop
 
 let i;
-const string = ['c is fun', 'python is cool', 'javascript is amazing'];
+const string = ['c is fun', 'python is cool', 'javascript is amazing\n'];
 
 for (i = 0; i < 3; i++) {
   console.log(string[i]);
